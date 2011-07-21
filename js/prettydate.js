587c37d4deb84fba6cbdd313e1320aba000cc621
Charles Smith <twohlix@gmail.com>
2011-07-21 15:32:01 -0400

    PrettyDate initial git hub add

---
+++ js/prettydate.js
@@ -0,0 +1,47 @@
+/*
+ * JavaScript Pretty Date
+ * Copyright (c) 2008 John Resig (jquery.com)
+ * Licensed under the MIT license.
+ * Modified 2011 Charles Smith (phase2technology.com)
+ */
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(time){
+	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+		diff = (((new Date()).getTime() - date.getTime()) / 1000),
+		day_diff = Math.floor(diff / 86400);
+			
+	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+		return;
+	
+  var monthNames = [ "January", "February", "March", "April", "May", "June",
+                     "July", "August", "September", "October", "November", "December" ];
+
+	return day_diff == 0 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff > 1 && monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+	
+}
+
+// If jQuery is included in the page, adds a jQuery plugin to handle it as well
+if ( typeof jQuery != "undefined" )
+	jQuery.fn.prettyDate = function(){
+		return this.each(function(){
+			var date = prettyDate(this.title);
+			if ( date )
+				jQuery(this).text( date );
+		});
+	};
+
+(function($){
+  $(document).ready(function(){
+    $('.prettydate').prettyDate();
+    setInterval(function(){ $('.prettydate').prettyDate(); }, 5000);
+  });
+}(jQuery));
